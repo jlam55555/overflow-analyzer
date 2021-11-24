1b5ed122c3c378b11cd8a78d7732e84effb37ecf
Jonathan Lam <jlam55555@gmail.com>
2021-11-24 02:22:35 -0500

    implemented basic parsing module from file in cpp

--- README.md
+++ README.md
@@ -6,53 +6,13 @@ Final project for ECE455 by Jonathan Lam and Daniel Tsarev
 
 ---
 
-# Overview
+### Overview
 TODO: talk about analysis
 
 ---
 
-# Build instructions
-This requires a standard OCaml installation with `opam`. The `dune` build system is used for running and testing.
-
-### Setup
+### Build instructions
 ```bash
-$ opam install dune base llvm    # dependencies
-$ opam install utop ounit2 odoc  # devdependencies
-```
-
-### Building
-```bash
-$ make build
-$ make run                       # build and run
-```
-
-##### Building documentation
-```bash
-$ make docs
-$ make show_docs                 # make and open in browser
-```
-
-##### Cleanup
-Clean up build artifacts
-```bash
-$ make clean
-```
-
-##### Running tests
-Build LLVM bytecode test cases:
-```bash
-$ make build_bc
-```
-
-Run tests:
-```bash
-$ dune test
-```
-
----
-
-# Development
-REPL using `utop`:
-```dune
-$ dune utop .
+$ make all  # build all, including LLVM bitcode input files
+$ make run  # run the example
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
-#include <iostream>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/Bitcode/BitcodeReader.h>
 #include <llvm/Support/MemoryBuffer.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
-        // Read in bitcode from file
-        cout << "Hello, world!" << endl;
-        
-        
-  //       let llctx = Llvm.global_context () in
-  // let llmem = Llvm.MemoryBuffer.of_file path in
-  // Llvm_bitreader.parse_bitcode llctx llmem
+        // Reference OCaml code (TODO: remove)
+        // let llctx = Llvm.global_context () in
+        // let llmem = Llvm.MemoryBuffer.of_file path in
+        // Llvm_bitreader.parse_bitcode llctx llmem
 
+        // Read in bitcode from file
+        llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> bufferOrErr
+                = llvm::MemoryBuffer::getFile("res/llvm/hello.bc");
+        if (auto err = llvm::errorCodeToError(bufferOrErr.getError())) {
+                llvm::errs() << llvm::toString(std::move(err)) << "\n";
+                return 1;
+        }
+        llvm::MemoryBufferRef memoryBuffer
+                = bufferOrErr.get().get()->getMemBufferRef();
+
+        // Build module from memory buffer
+        llvm::LLVMContext context{};
+        llvm::Expected<std::unique_ptr<llvm::Module>> moduleOrErr
+                = llvm::parseBitcodeFile(memoryBuffer, context);
+        if (auto err = moduleOrErr.takeError()) {
+                llvm::errs() << llvm::toString(std::move(err)) << "\n";
+                return 1;
+        }
+        llvm::Module *module = moduleOrErr.get().get();
+
+        llvm::outs() << "Done.\n";
         return 0;
 }
