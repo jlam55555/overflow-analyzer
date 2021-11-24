c3e5c16cb08fb0af4a5f0f9fc606a51a7ca7b6b8
Jonathan Lam <jlam55555@gmail.com>
2021-11-24 16:11:52 -0500

    added function to get the list of functions from a module

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,23 @@
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Support/Error.h>
 
-int err_panic(llvm::Error& err, int code) {
+// Note: avoid `using namespace *` and `auto` typing so that namespaces and
+// types are very explicit. Good for avoiding confusion for learning purposes.
+
+/**
+ * Helper function to exit the program due to a `llvm::Error`. Will exit
+ * the entire program with the given return code.
+ */
+void err_panic(llvm::Error& err, int code) {
         llvm::errs() << llvm::toString(std::move(err)) << "\n";
-        return code;
+        exit(code);
 }
 
+/**
+ * Returns a (managed) reference to a `llvm::Module`, or error. The scope of
+ * the `llvm::LLVMContext` should outlive the module, since it manages the
+ * memory allocations of the module.
+ */
 llvm::Expected<std::unique_ptr<llvm::Module>>
 getModuleFromFile(std::string& filename, llvm::LLVMContext& context) {
         // Read in bitcode from file
@@ -22,6 +34,25 @@ getModuleFromFile(std::string& filename, llvm::LLVMContext& context) {
                                       ->getMemBufferRef(), context);
 }
 
+/**
+ * Returns a vector of (unmanaged) pointers to functions from a `llvm::Module`.
+ */
+std::vector<llvm::Function *>
+getFunctionsFromModule(std::unique_ptr<llvm::Module>& module) {
+        std::vector<llvm::Function *> fns{};
+        
+        for (llvm::Module::FunctionListType::iterator it
+                     = module->functions().begin();
+             it != module->functions().end(); ++it) {
+                fns.push_back(&*it);
+        }
+
+        return fns;
+}
+
+/**
+ * Begins the overflow analysis.
+ */
 int main(int argc, char **argv) {
         // Get module from path
         std::string path = "res/llvm/hello.bc";
@@ -29,10 +60,21 @@ int main(int argc, char **argv) {
         llvm::Expected<std::unique_ptr<llvm::Module>> moduleOrErr
                 = getModuleFromFile(path, context);
         if (llvm::Error err = moduleOrErr.takeError()) {
-                return err_panic(err, -1);
+                err_panic(err, -1);
         }
+        std::unique_ptr<llvm::Module> module = std::move(moduleOrErr.get());
 
-        // TODO: grab all the LLVM data
+        // Print module (for debugging)
+        // module->print(llvm::errs(), nullptr);
+
+        // grab LLVM functions, basic blocks, instructions, lvars
+        std::vector<llvm::Function *> fns = getFunctionsFromModule(module);
+
+        // Print functions (for debugging)
+        // for (auto f : fns) {
+        //         f->print(llvm::errs(), nullptr);
+        // }
+
         // TODO: perform the analysis
 
         // Done.
